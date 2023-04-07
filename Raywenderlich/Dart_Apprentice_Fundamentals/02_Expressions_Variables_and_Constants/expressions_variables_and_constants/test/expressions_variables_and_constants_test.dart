d6ef9552bd68a70ce4348c7fbe15243167438a24
Youngho <byuncorry@hanmail.net>
2023-04-07 22:21:01 +0900

    Finished Chapter 02: Expressions Variables & Constants

---
+++ Raywenderlich/Dart_Apprentice_Fundamentals/02_Expressions_Variables_and_Constants/expressions_variables_and_constants/test/expressions_variables_and_constants_test.dart
@@ -0,0 +1,8 @@
+import 'package:expressions_variables_and_constants/expressions_variables_and_constants.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
